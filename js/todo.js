01272360fd9b59b264e54d2ac14721b7993d4c0d
ethan <noahkim.j@gmail.com>
2024-10-26 13:11:15 +0900

    7.1 Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function printTodo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo
+    console.log(li);
+
+    toDoList.append(li);
+}
+
 function handleTodoSummit(event) {
     event.preventDefault();
     console.log(toDoInput.value);
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    printTodo(newTodo)
 }
 
 toDoForm.addEventListener("submit", handleTodoSummit);
