2bebd53f67d5a4666588a5b03e1c8498167c3294
ethan <noahkim.j@gmail.com>
2024-10-26 19:34:32 +0900

    weather api

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,25 @@
 
-function onGeoOk(position){
+const API_KEY = "6f60d56f7350e2525526dca0a9e584e7"
+
+function onGeoOk(position) {
     const lat = position.coords.latitude
-    const lng = position.coords.longitude
-    console.log("You live in ", lat, lng);
+    const lon = position.coords.longitude
+    console.log("You live in ", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.name, data.weather[0].main);
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
-function onGeoError(){
+function onGeoError() {
     alert("Can't find you. No weather for you");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
